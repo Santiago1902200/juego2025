5f233df82e0c72bc8cb37cf54b0cb6323ef873e7
Santiago1902200 <santiagocd00@gmail.com>
2025-03-26 16:14:25 -0500

    Update script.js 26-03

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
- const firebaseConfig = {
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
     apiKey: "AIzaSyCkorvIgkM-xZ4YQYmpn_Zi-uwe_CMm99U",
     authDomain: "juego-unico.firebaseapp.com",
+    databaseURL: "https://juego-unico-default-rtdb.firebaseio.com",
     projectId: "juego-unico",
     storageBucket: "juego-unico.firebasestorage.app",
     messagingSenderId: "1072325060700",
     appId: "1:1072325060700:web:e061578857ec1e7dbede99"
   };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
 
 const numeroSecreto = Math.floor(Math.random() * 100) + 1;
 
